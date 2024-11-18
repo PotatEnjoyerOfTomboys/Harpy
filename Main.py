34ef04ea64523ca9698f1acd4be5777ed7268e46
Cedric <cedric.trottier@monboreal.ca>
2024-11-18 17:00:13 -0500

    Updated Main.py, added docekrfile

--- Main.py
+++ Main.py
@@ -1,3 +1,25 @@
+import json
+
+
+class Harpy:
+    def __init__(self, func_list):
+        # Give an option to load script lists from a JSON file
+        self.func_list = func_list
+        # Make it go through the list to check for strings or invalid functions
+
+    def act(self):
+        while True:
+            print("Harpy Script ")
+            for count, x in enumerate(self.func_list):
+                print(f'[{count}] {x.__name__}')
+
+            func_to_use = get_user_number()
+            if -1 < func_to_use < len(self.func_list):
+                self.func_list[func_to_use]()
+            else:
+                print("Error: out of index")
+
+
 # General use functions
 def get_user_number(text="Enter number: ", num_type=int):
     while True:
@@ -9,22 +31,32 @@ def get_user_number(text="Enter number: ", num_type=int):
     #
 
 
+def dict_to_json(filename, dictionary):
+    # This function make stuff go in a json file
+    with open(filename, "w", encoding='utf-8') as outfile:
+        # json.dump(dictionary, outfile)
+        json.dump(dictionary, outfile, indent=1)
 
+
+def get_from_json(filename, requested_info):
+    # This function gets you stuff from a json file
+    with open(filename, encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        if requested_info == "Everything":
+            return data
+        return data[requested_info]
+
+
+# |Scripts|-------------------------------------------------------------------------------------------------------------
+# |Default script|------------------------------------------------------------------------------------------------------
 def leave():
-    input("Exiting.")
+    input("Press enter to quit: ")
     quit()
 
 
 if __name__ == "__main__":
-    func_list = [leave]
-    while True:
-        print("Script list")
-        for count, x in enumerate(func_list):
-            print(f'[{count}] {x.__name__}')
+    Harpy(
+        [leave]
+    ).act()
 
-        func_to_use = get_user_number()
-        if -1 < func_to_use < len(func_list):
-            func_list[func_to_use]()
-        else:
-            print("Error: out of index")
 
